b3dbe9924c400a24f6acdddd8ea55a6325f13150
Daniel Barber <github@danbarber.me>
2019-11-05 22:03:10 -0500

    Add rules

---
+++ keyboards/thevankeyboards/minivan/keymaps/danbee/rules.mk
@@ -0,0 +1,5 @@
+BOOTMAGIC_ENABLE = full      # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+KEYBOARD_LOCK_ENABLE = yes  # Allow locking of keyboard via magic key
+NKRO_ENABLE = yes           # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
